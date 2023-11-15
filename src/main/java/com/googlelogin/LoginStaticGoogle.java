ec4593fd4fdd351bf77c2c250a878477b616eae8
tazin7802 <rakibulbari@gmail.com>
2023-11-15 13:36:47 -0600

    Update with Static Class

---
+++ src/main/java/com/googlelogin/LoginStaticGoogle.java
@@ -0,0 +1,13 @@
+package com.googlelogin;
+
+public class LoginStaticGoogle {
+	
+	public static void getstaticLogin() {
+		
+		System.out.println("This static Login ");
+	}
+	
+public static void main(String[] args) {
+	LoginStaticGoogle.getstaticLogin();
+}
+}
