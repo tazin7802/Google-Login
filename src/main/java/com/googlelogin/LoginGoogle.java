4dcef7e4fcfd166d011bc1f75fc09cfb3d4f6221
tazin7802 <rakibulbari@gmail.com>
2023-11-15 13:29:26 -0600

    This is my first push

---
+++ src/main/java/com/googlelogin/LoginGoogle.java
@@ -0,0 +1,13 @@
+package com.googlelogin;
+
+public class LoginGoogle {
+	
+	public void getLogin() {
+		
+		System.out.println("This is Google Login");
+	}
+public static void main(String[] args) {
+	LoginGoogle obj = new LoginGoogle();
+	obj.getLogin();
+}
+}
